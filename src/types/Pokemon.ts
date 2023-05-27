846792e9f9da3ce93c8b61b1bcf653bc8317a66c
Sam-Dunlap <sam.dunlap15@gmail.com>
2023-05-27 01:46:57 -0700

    add more verbose stats

--- src/types/Pokemon.ts
+++ src/types/Pokemon.ts
@@ -1,83 +1,101 @@
+import Teratype from "./Teratype";
+
 class Pokemon {
-	name: string;
-	realName: string;
-	status: string;
-	statusInflictor: string;
-	statusType: string;
-	otherAffliction: { [key: string]: string };
-	causeOfDeath: string;
-	currentDKills: number;
-	directKills: number;
-	currentPKills: number;
-	passiveKills: number;
-	isDead: boolean;
-	killer: string;
-	hasSubstitute: boolean;
+    name: string;
+    status: string;
+    statusInflictor: string;
+    statusType: string;
+    otherAffliction: { [key: string]: string };
+    causeOfDeath: string;
+    currentDKills: number;
+    directKills: number;
+    currentPKills: number;
+    passiveKills: number;
+    isDead: boolean;
+    killer: string;
+    hasSubstitute: boolean;
+    turnsOnBattlefield: number;
+    terastallize: Teratype;
+    brought: boolean;
+    lead: boolean;
 
-	constructor(pokemonName: string, realName?: string) {
-		this.name = pokemonName;
-		this.realName = realName || pokemonName;
-		this.status = 'n/a';
-		this.statusInflictor = '';
-		this.statusType = ''; //Passive or Direct or undefined
-		this.otherAffliction = {}; //Like Leech Seed and stuff
-		this.causeOfDeath = 'n/a';
-		this.currentDKills = 0;
-		this.directKills = 0;
-		this.currentPKills = 0;
-		this.passiveKills = 0;
-		this.isDead = false;
-		this.killer = '';
-		this.hasSubstitute = false;
-	}
+    constructor(name: string) {
+        this.name = name;
+        this.status = "n/a";
+        this.statusInflictor = "";
+        this.statusType = ""; //Passive or Direct or undefined
+        this.otherAffliction = {}; //Like Leech Seed and stuff
+        this.causeOfDeath = "n/a";
+        this.currentDKills = 0;
+        this.directKills = 0;
+        this.currentPKills = 0;
+        this.passiveKills = 0;
+        this.isDead = false;
+        this.killer = "";
+        this.hasSubstitute = false;
+        this.turnsOnBattlefield = 0;
+        this.terastallize = "";
+        this.brought = false;
+        this.lead = false;
+    }
 
-	//If the pokemon gets poisoned, burned, etc.
-	statusEffect(statusInflicted: string, statusInflictor: string, statusType: string) {
-		this.status = statusInflicted;
-		this.statusInflictor = statusInflictor;
-		this.statusType = statusType;
-	}
+    //If the pokemon gets poisoned, burned, etc.
+    statusEffect(
+        statusInflicted: string,
+        statusInflictor: string,
+        statusType: string
+    ) {
+        this.status = statusInflicted;
+        this.statusInflictor = statusInflictor;
+        this.statusType = statusType;
+    }
 
-	//If the pokemon gets healed with healing bell, aromatherapy, etc.
-	statusFix() {
-		this.status = 'n/a';
-		this.statusInflictor = '';
-		this.statusType = '';
-	}
+    //If the pokemon gets healed with heal bell, aromatherapy, etc.
+    statusFix(): void {
+        this.status = "n/a";
+        this.statusInflictor = "";
+        this.statusType = "";
+    }
 
-	clearAfflictions() {
-		this.otherAffliction = {};
-	}
+    clearAfflictions() {
+        this.otherAffliction = {};
+    }
 
-	//When the pokemon has killed another pokemon in battle
-	killed(deathJson: { killer: string; isPassive: boolean }) {
-		if (deathJson.killer) {
-			if (deathJson.isPassive) this.currentPKills++;
-			else this.currentDKills++;
-		}
-	}
+    //When the pokemon has killed another pokemon in battle
+    killed(deathJson: { killer: string; isPassive: boolean }) {
+        if (deathJson.killer) {
+            if (deathJson.isPassive) this.currentPKills++;
+            else this.currentDKills++;
+        }
+    }
 
-	unkilled(isPassive?: boolean) {
-		if (isPassive) this.currentPKills--;
-		else this.currentDKills--;
-	}
+    unkilled(isPassive?: boolean) {
+        if (isPassive) this.currentPKills--;
+        else this.currentDKills--;
+    }
 
-	//Run when the pokemon has died in battle
-	died(causeOfDeath: string, killer: string, isPassive: boolean) {
-		this.causeOfDeath = causeOfDeath;
-		this.killer = killer ? killer : '';
-		this.isDead = true;
+    //Run when the pokemon has died in battle
+    died(causeOfDeath: string, killer: string, isPassive: boolean) {
+        this.causeOfDeath = causeOfDeath;
+        this.killer = killer ? killer : "";
+        this.isDead = true;
 
-		return {
-			killer: killer,
-			isPassive: isPassive,
-		};
-	}
+        return {
+            killer: killer,
+            isPassive: isPassive,
+        };
+    }
 
-	undied() {
-		this.causeOfDeath = '';
-		this.isDead = false;
-	}
+    undied() {
+        this.causeOfDeath = "";
+        this.isDead = false;
+    }
+
+    incrementTurn() {
+        if (!this.isDead) {
+            this.turnsOnBattlefield++;
+        }
+    }
 }
 
 export default Pokemon;
