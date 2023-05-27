27457f11678905e7cebc2772103e9b261b5dd356
Sam-Dunlap <sam.dunlap15@gmail.com>
2023-05-27 01:48:45 -0700

    one of 18 types or an empty string. for Pokemon.ts

---
+++ src/types/Teratype.ts
@@ -0,0 +1,22 @@
+type Teratype =
+    | ""
+    | "Grass"
+    | "Fire"
+    | "Water"
+    | "Electric"
+    | "Rock"
+    | "Ground"
+    | "Steel"
+    | "Fairy"
+    | "Poison"
+    | "Psychic"
+    | "Dark"
+    | "Ghost"
+    | "Ice"
+    | "Normal"
+    | "Flying"
+    | "Fighting"
+    | "Dragon"
+    | "Bug";
+
+export default Teratype;
