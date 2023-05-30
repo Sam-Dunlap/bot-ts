4e84ca3a9d2ad3e05d46e6f82db15d85ed53a006
Sam-Dunlap <sam.dunlap15@gmail.com>
2023-05-29 22:43:46 -0700

    add timestamp, change output data types

--- src/utils/funcs.ts
+++ src/utils/funcs.ts
@@ -9,33 +9,43 @@ const randomElement = (list: string[]) => {
     return list[Math.round(Math.random() * (list.length - 1))];
 };
 
-const formatToCSV = (results: RawStats): string => {
+const formatToCSV = (results: RawStats, timeStamp: Date): string => {
+    const day = timeStamp.getDate();
+    const month = timeStamp.getMonth() + 1;
+    const year = timeStamp.getFullYear();
+    const fullDate = `${year}-${month >= 10 ? month : `0${month}`}-${
+        day >= 10 ? day : `0${day}`
+    }`;
     let returnString = "";
 
     const p1pokes = results.p1Pokemon;
     for (const key in p1pokes) {
         const pokemon = p1pokes[key];
-        returnString += `${results.info.replay},${results.playerNames[0]},${
-            pokemon.name
-        },${pokemon.status},${pokemon.statusInflictor},${pokemon.directKills},${
-            pokemon.passiveKills
-        },${pokemon.isDead},${pokemon.killer},${pokemon.terastallize},${
-            pokemon.brought
+        returnString += `${fullDate},${results.info.replay},${
+            results.playerNames[0]
+        },${pokemon.name},${pokemon.status === "n/a" ? "" : pokemon.status},${
+            pokemon.statusInflictor
+        },${pokemon.directKills},${pokemon.passiveKills},${
+            pokemon.isDead ? 1 : 0
+        },${pokemon.killer},${pokemon.terastallize},${
+            pokemon.brought ? 1 : 0
         },${results.info.winner === results.playerNames[0] ? 1 : 0},${
-            pokemon.lead
+            pokemon.lead ? 1 : 0
         },${pokemon.turnsOnBattlefield}\n`;
     }
     const p2pokes = results.p2Pokemon;
     for (const key in p2pokes) {
         const pokemon = p2pokes[key];
-        returnString += `${results.info.replay},${results.playerNames[1]},${
-            pokemon.name
-        },${pokemon.status},${pokemon.statusInflictor},${pokemon.directKills},${
-            pokemon.passiveKills
-        },${pokemon.isDead},${pokemon.killer},${pokemon.terastallize},${
-            pokemon.brought
+        returnString += `${fullDate},${results.info.replay},${
+            results.playerNames[1]
+        },${pokemon.name},${pokemon.status === "n/a" ? "" : pokemon.status},${
+            pokemon.statusInflictor
+        },${pokemon.directKills},${pokemon.passiveKills},${
+            pokemon.isDead ? 1 : 0
+        },${pokemon.killer},${pokemon.terastallize},${
+            pokemon.brought ? 1 : 0
         },${results.info.winner === results.playerNames[1] ? 1 : 0},${
-            pokemon.lead
+            pokemon.lead ? 1 : 0
         },${pokemon.turnsOnBattlefield}\n`;
     }
     return returnString;
